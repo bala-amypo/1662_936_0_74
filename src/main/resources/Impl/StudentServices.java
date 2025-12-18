62531991d4359d96644b54d77a2b7a28305b3400
bala-amypo <bala@amypo.in>
2025-12-18 05:28:17 +0000

    User commit

--- src/main/resources/Impl/StudentServices.java
+++ src/main/resources/Impl/StudentServices.java
@@ -1,16 +1,4 @@
 Package com.example.demo.services;
 
-import com.example.demo 
-import org.springframework.web.blind.annotation.GetMapping;
-@RestController
-public class Studentcontroller{
-@Autowired Studentservices services;
-@postMapping("/post")
-public StudentEntity postval(@RequestBody StudentEntity entity){
-    return services.postdata(entity);
-}
-@GetMapping("/get")
-public List<StudentEntity>getval(){
-    return services.getAllData()
-}
-}
+@override
+public stu
