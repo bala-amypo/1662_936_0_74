06848caf9ff79ae7499081299d3443f0d1865fdc
bala-amypo <bala@amypo.in>
2025-12-18 06:13:17 +0000

    User commit

---
+++ src/main/resources/Impl/Studentservicesimpl.java
@@ -0,0 +1,15 @@
+Package com.example.demo.services;
+
+import com.example.demo 
+import org.springframework.web.blind.annotation.GetMapping;
+@RestController
+public class Studentcontroller{
+@Autowired Studentservices services;
+@postMapping("/post")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     Package com.example.demo.services;
+
+import com.example.demo 
+import org.springframework.web.blind.annotation.GetMapping;
+@RestController
+public class Studentcontroller{
+@Autowired Studentservices services;
+@postMapping("/post")0
