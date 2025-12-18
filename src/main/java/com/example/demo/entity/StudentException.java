5a85588401f81402ab3624db7c40d78ddb3e7907
bala-amypo <bala@amypo.in>
2025-12-18 10:43:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/StudentException.java
@@ -0,0 +1,54 @@
+package com.example.demo.entity
+
+import org.springframework.data.jpa.respository
+
+@Repository
+pubilc interface StudentRepository extends JpaRepository<StudentEntity,Integer>
+public class StudentEntity{
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    private String createAt;
+
+    public void setId(Integer){
+        this.id=id;                                                                                       
+    }
+    public Integer getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public Date getCreateAt(){
+        return CreacteAt;
+    }
+    public void setCreateAt(Date creatAt){
+        this.createAt = createAt;
+    }
+    public void StudentEntity(){
+        
+    }
+    public StudentEntity(Integer id,String name,String email,String creatAt){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.creatAt = creatAt; 
+    }
+}
