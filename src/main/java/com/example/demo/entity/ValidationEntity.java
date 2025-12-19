460306678d004f003b57bc2600c71ce4b0315301
bala-amypo <bala@amypo.in>
2025-12-19 07:54:00 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,32 +1,45 @@
-package com.example.demo.Entity
-import jakarta.presistence.Entity;
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import java.time.localDateTime;
 
 @Entity
+@Data
+@AllArgsConstructor
+
 public class ValidationEntity{
     @Id
-    @GenerateValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @NotNull
+    @Size(min=2,max=0,message="must be 2 to 10 character")
     private String username;
-     private String email ;
-      private String password;
-       private int age;
-       @Size(min=2,max=10,messsage="must be 2 to 10 character")
-       prinvate String username;
-       @Email(message="Email is not valid")
-       private String Email
-       @
-       private Sting password;
+    @Email(message="Email is not valid")
+    private String email;
+    @Size(min=2,max=0,message="must be 2 to 10 character")
+    @NotNull(message="password is mandatory")
+    private String password;
+    @Max(30)
+    @Positive(message="Age is be a positive number")
+    private int age;
 
-       private int age; 
-@Entity
-
-
-
-
-
+private LocalDateTime createAt;
+private LocalDateTime updateAt;
 }
-
-
 
 
